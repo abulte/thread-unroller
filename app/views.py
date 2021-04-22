03308df9e367d8c8625379052adf22da056ef48a
Alexandre Bulté <alexandre@bulte.net>
2021-04-22 22:47:31 +0200

    cleanup

--- app/views.py
+++ app/views.py
@@ -1,32 +1,33 @@
-from flask import render_template, send_file, request, send_from_directory
-from app.modules.unthread import *
-import logging, os, io
+import io
+import logging
+import os
+
+from flask import render_template, send_file, request
+from app.modules.unthread import unthread
 
 from app import app
 
-logging.basicConfig(filename='bot.log', filemode='w', level=logging.WARNING)
 
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html', files=[])
+    if request.method == "GET":
+        return render_template("index.html", files=[])
     else:
-        url = request.form['tweetUrl'] if 'tweetUrl' in request.values else None
+        url = request.form["tweetUrl"] if "tweetUrl" in request.values else None
         out = unthread(url)
         filename = os.path.splitext(os.path.basename(out))
-        return render_template('index.html', files=[filename[0]])
+        return render_template("index.html", files=[filename[0]])
 
 
-@app.route('/download', methods=['POST'])
+@app.route("/download", methods=["POST"])
 def download():
     file_base = [x for x in request.form][0]
-    fn = '{}.txt'.format(file_base)
-    fp = os.path.abspath('files/{}'.format(fn))
+    fn = "{}.txt".format(file_base)
+    fp = os.path.abspath("files/{}".format(fn))
 
     try:
         download_bytes = io.BytesIO()
-        with open(fp, 'rb') as f:
+        with open(fp, "rb") as f:
             download_bytes.write(f.read())
         download_bytes.seek(0)
         os.remove(fp)
